351bea0834a259128f0a7e826b750a98c5a8176e
paspekov34 <nikpaspekov17@mail.ru>
2024-09-18 23:25:33 +0300

    Add files via upload

---
+++ Prostoy_unittest.py
@@ -0,0 +1,26 @@
+from Prosto_test import Runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner("Ваня")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("Макс")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Оля")
+        runner2 = Runner("Сергей")
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
